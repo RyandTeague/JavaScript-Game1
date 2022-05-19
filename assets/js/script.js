667ecc365e16db76c22f82f29338f0869a8c87f7
RyandTeague <ryanteague@hotmail.co.uk>
2022-05-19 12:41:23 +0000

    add reactivity to page and start assigning variables in java

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let pattern = [];
+let playerPattern = [];
+let score = 0;
+
+
+function runGame() {
+    
+}
+
+function nextLevel () {
+    ++score;
+}
+
+function pathBuild() {
+    if (score = 1) {
+        const tiles = ['1', '2', '3'];
+        const random = tiles[Math.floor(Math.random() * 3 )];
+        return random;
+    } else if (score = 2) {
+        
+    }
+}
